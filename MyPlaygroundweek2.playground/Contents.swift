fbf39d0e1bc224138e08a5fc950c7d6eac337451
Nouf-123 <nllllr90@gmail.com>
2024-08-01 20:05:18 +0300

    hi

---
+++ MyPlaygroundweek2.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var mathMark = 90
+
+if (mathMark >= 90){
+    print("Exellent")
+}
+else if (mathMark >= 80){
+    print("Very good")
+}
+else if (mathMark >= 70){
+    print("Good")
+}
+else if (mathMark >= 60){
+    print("Pass")
+}
+else{
+    print("Fail")
+}
